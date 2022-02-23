cdd81b80311a2c6c7dc746d27923924c092582f3
Bhavy Patel <bppatel16@myseneca.ca>
2022-02-23 18:58:41 -0500

    update

---
+++ My Codes/c++/Header_and_operator.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+//if you want to see the what is iostram , click on it with ctrl
+//iostram is system file which give functionality to code
+
+// There are two types of header file
+// 1)system Header ----->comes with Compiler
+// 2)User defined ------>Written by programmer
+
+// you can include like this #include "car.h"
+// It should be added in your current directory
+
+//cpp reference named website is one of the website to watch header file
+
+
+using namespace std;
+
+
+int main(void){
+    cout<<"This Is the Code for understanding Header file and Operator"<<endl;
+
+
+//-----------------------Operators--------------------------------------------
+// Following are the operators:
+//Arithmetic
+int a=4,b=5;
+cout<< "The value of a + b is "<<a+b<<endl;
+cout<<"The value of a b is "<<a-b<<endl;
+cout<<" The value of a * b is "<<a*b<<endl;
+cout<<"The value of a / b is "<<a/b<<endl;
+cout<<"The value of a % b is "<<a % b<<endl;
+cout<<"The value of a++ is "<<a++<<endl;
+cout<<"The value of a-- is "<<a--<<endl;
+cout<<" The value of ++a is "<<++a<<endl;
+cout<<" The value of --a is "<<--a<<endl;
+
+
+return 0;
+}
---
+++ My Codes/c++/HelloWold.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(void){
+cout<<"HelloWorld!"<<endl;
+//endl-----------> for the end of line '\n'
+int m;
+//to print use:---------> cout
+//for input use:--------> cin
+cout<<"Enter the number: "; cin>>m;
+cout<<"You have Enter the number: "<<m<<endl;
+return 0;
+}
